0d4906a8ea13fa3952874788827f08188a8b4a93
Philip Schinis <p.schinis@gmail.com>
2024-01-11 17:28:20 -0500

    remove unnecessary imports

--- src/android/net/texh/cordovapluginstepcounter/StepCounterService.java
+++ src/android/net/texh/cordovapluginstepcounter/StepCounterService.java
@@ -24,34 +24,21 @@ package net.texh.cordovapluginstepcounter;
 
  */
 
-import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.app.Service;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.graphics.BitmapFactory;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Build;
-import android.os.IBinder;
-import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 import android.util.Log;
-import android.widget.RemoteViews;
 import java.util.Timer;
 import java.util.TimerTask;
-
-import com.strongrfastr.cordova.R;
-
-import java.util.Locale;
 
 public class StepCounterService extends JobService {
     private static int NOTIFICATION_ID = 777;
